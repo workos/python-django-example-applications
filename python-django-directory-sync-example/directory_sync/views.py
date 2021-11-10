7296c1b676170478f600aebd33b110df971dadf5
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-11-10 16:57:15 -0700

    Update UI/UX to utilize GET directory endpoint

--- python-django-directory-sync-example/directory_sync/views.py
+++ python-django-directory-sync-example/directory_sync/views.py
@@ -12,14 +12,23 @@ directory_id = os.getenv('DIRECTORY_ID')  # Follow the WorkOS guide to get this
 
 def get_home(request):
     print('request', request)
-    return render(request, 'directory_sync/home.html')
+    directories = workos.client.directory_sync.list_directories()
+    directories = directories['data']
+    return render(request, 'directory_sync/home.html', {"directories": directories})
+
+def get_directory(request):
+    directory_id = request.GET['id']
+    directory = workos.client.directory_sync.get_directory(directory_id)
+    return render(request, 'directory_sync/directory.html', {"directory_id": directory_id, "directory": directory})
 
 def get_directory_users(request):
+    directory_id = request.GET['id']
     users = workos.client.directory_sync.list_users(directory=directory_id)
     return render(request, 'directory_sync/users.html', {"users": users})
 
 
 def get_directory_groups(request):
+    directory_id = request.GET['id']
     groups = workos.client.directory_sync.list_groups(directory=directory_id)
     return render(request, 'directory_sync/groups.html', {"groups": groups})
 
