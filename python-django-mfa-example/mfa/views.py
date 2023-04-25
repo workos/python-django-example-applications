51e5a6ee7499815a1e896333770957cf4b3d2e2f
Edita Ignot <edignot@gmail.com>
2023-04-25 16:55:40 -0600

    totp factor detaqils updated

--- python-django-mfa-example/mfa/views.py
+++ python-django-mfa-example/mfa/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 import json
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 
 workos.api_key = os.getenv("WORKOS_API_KEY")
@@ -38,38 +38,6 @@ def list_factors(request):
 
 def enroll_factor_details(request):
     return render(request, "mfa/enroll_factor_details.html")
-
-
-@csrf_exempt
-def enroll_factor(request):
-    factor_type = request.POST["type"]
-
-    if factor_type == "sms":
-        factor_type = "sms"
-        phone_number = request.POST["phone_number"]
-        new_factor = workos.client.mfa.enroll_factor(
-            type=factor_type, phone_number=phone_number
-        )
-        if request.session.get("factor_list"):
-            request.session["factor_list"].append(new_factor)
-        else:
-            request.session["factor_list"] = [new_factor]
-
-    if factor_type == "totp":
-        factor_type = "totp"
-        totp_issuer = request.POST["totp_issuer"]
-        totp_user = request.POST["totp_user"]
-        new_factor = workos.client.mfa.enroll_factor(
-            type=factor_type, totp_issuer=totp_issuer, totp_user=totp_user
-        )
-        if request.session.get("factor_list") != None:
-            new_session_list = request.session["factor_list"]
-            new_session_list.append(new_factor)
-            request.session["factor_list"] = new_session_list
-        else:
-            request.session["factor_list"] = [new_factor]
-
-    return redirect("list_factors")
 
 
 @csrf_exempt
@@ -109,10 +77,7 @@ def enroll_totp_factor(request):
     else:
         request.session["factor_list"] = [new_factor]
 
-    print(new_factor)
-
-    response_data = {'message': 'Success'}
-    return JsonResponse(response_data, status=200)
+    return HttpResponse(new_factor['totp']['qr_code'])
 
 
 def factor_detail(request):
@@ -125,9 +90,6 @@ def factor_detail(request):
         if factor["type"] == "sms":
             phone_number = factor["sms"]["phone_number"]
 
-        if factor["type"] == "totp":
-            request.session["current_factor_qr"] = factor["totp"]["qr_code"]
-
     request.session["current_factor"] = fullFactor["id"]
     request.session["current_factor_type"] = fullFactor["type"]
 
@@ -137,7 +99,6 @@ def factor_detail(request):
         {
             "factor": fullFactor,
             "phone_number": phone_number,
-            "qr_code": request.session["current_factor_qr"],
         },
     )
 
